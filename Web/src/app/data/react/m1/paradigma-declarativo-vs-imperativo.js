219d859250f87eb4e653c8a469f13e8dc9ab0a3e
Caiorossi00 <caio_rossi56@hotmail.com>
2026-02-21 09:38:11 -0300

    Adicionar pasta Web ao repositório principal

---
+++ Web/src/app/data/react/m1/paradigma-declarativo-vs-imperativo.js
@@ -0,0 +1,20 @@
+export default {
+  slug: "paradigma-declarativo-vs-imperativo",
+  title: "Paradigma declarativo vs imperativo",
+  content: `
+  Programação imperativa descreve o "como".
+  Programação declarativa descreve o "o que".
+  
+  Imperativo:
+  - Seleciona elemento
+  - Modifica DOM
+  - Atualiza manualmente
+  
+  Declarativo (React):
+  - Descreve o estado desejado
+  - A UI é recalculada automaticamente
+  
+  React não manipula interface.
+  Ele descreve estados finais possíveis da interface.
+    `,
+};
